7c1e6c878f565835a957c004fe929583f27b12f6
Anthony Petrov <beavean@gmail.com>
2022-10-15 14:20:14 +0300

    initial commit (no storyboards)

---
+++ Spotify/Container.swift
@@ -0,0 +1,15 @@
+//
+//  Container.swift
+//  Spotify
+//
+//  Created by Beavean on 11.10.2022.
+//
+
+import UIKit
+
+class Container: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
