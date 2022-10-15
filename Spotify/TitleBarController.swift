d18e5c42bf91387362b2eff12aae111092618be7
Anthony Petrov <beavean@gmail.com>
2022-10-15 14:21:01 +0300

    add TitleBarController with additional controllers

---
+++ Spotify/TitleBarController.swift
@@ -0,0 +1,177 @@
+//
+//  TitleBarController.swift
+//  Spotify
+//
+//  Created by Beavean on 11.10.2022.
+//
+
+import UIKit
+
+class MusicViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
+
+class PodcastViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+}
+
+class TitleBarController: UIViewController {
+    
+    //MARK: - Properties
+    
+    var musicBarButtonItem: UIBarButtonItem!
+    var podcastBarButtonItem: UIBarButtonItem!
+    let container = Container()
+    let viewControllers: [UIViewController] = [MusicViewController(), PodcastViewController()]
+    
+    //MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavBar()
+        setupViews()
+    }
+    
+    private func setupNavBar() {
+        navigationItem.leftBarButtonItems = [musicBarButtonItem, podcastBarButtonItem]
+        let img = UIImage()
+        self.navigationController?.navigationBar.shadowImage = img
+        self.navigationController?.navigationBar.setBackgroundImage(img, for: .default)
+        self.navigationController?.navigationBar.isTranslucent = false
+    }
+    
+    private func setupViews() {
+        guard let containerView = container.view else { return }
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.backgroundColor = .systemOrange
+        view.addSubview(containerView)
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor, multiplier: 2),
+            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            containerView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        musicTapped()
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+        musicBarButtonItem = makeBarButtonItem(text: "Music", selector: #selector(musicTapped))
+        podcastBarButtonItem = makeBarButtonItem(text: "Podcasts", selector: #selector(podcastTapped))
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func makeBarButtonItem(text: String, selector: Selector) -> UIBarButtonItem {
+        var configuration = UIButton.Configuration.plain()
+        configuration.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: -8, bottom: 0, trailing: 0)
+        
+        let button = UIButton(configuration: configuration)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: selector, for: .primaryActionTriggered)
+        
+        let attributes = [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .largeTitle).withTraits(traits: [.traitBold]), NSAttributedString.Key.foregroundColor: UIColor.label]
+        
+        let attributedText = NSMutableAttributedString(string: text, attributes: attributes)
+        
+        button.setAttributedTitle(attributedText, for: .normal)
+        
+        let barButtonItem = UIBarButtonItem(customView: button)
+        return barButtonItem
+    }
+    
+    @objc private func musicTapped() {
+        if container.children.first == viewControllers[0] { return }
+        container.add(viewControllers[0])
+        animateTransition(fromVC: viewControllers[1], toVC: viewControllers[0]) { [weak self] success in
+            self?.viewControllers[1].remove()
+        }
+        UIView.animate(withDuration: 0.5) {
+            self.musicBarButtonItem.customView?.alpha = 1.0
+            self.podcastBarButtonItem.customView?.alpha = 0.5
+        }
+    }
+    
+    @objc private func podcastTapped() {
+        if container.children.first == viewControllers[1] { return }
+        container.add(viewControllers[1])
+        animateTransition(fromVC: viewControllers[0], toVC: viewControllers[1]) { [weak self]  success in
+            self?.viewControllers[0].remove()
+        }
+        UIView.animate(withDuration: 0.5) {
+            self.musicBarButtonItem.customView?.alpha = 0.5
+            self.podcastBarButtonItem.customView?.alpha = 1
+        }
+    }
+    
+    func animateTransition(fromVC: UIViewController, toVC: UIViewController, completion: @escaping ((Bool) -> Void)) {
+        guard
+            let fromView = fromVC.view,
+            let fromIndex = getIndex(forViewController: fromVC),
+            let toView = toVC.view,
+            let toIndex = getIndex(forViewController: toVC)
+        else {
+            return
+        }
+        
+        let frame = fromVC.view.frame
+        var fromFrameEnd = frame
+        var toFrameStart = frame
+        fromFrameEnd.origin.x = toIndex > fromIndex ? frame.origin.x - frame.width : frame.origin.x + frame.width
+        toFrameStart.origin.x = toIndex > fromIndex ? frame.origin.x + frame.width : frame.origin.x - frame.width
+        toView.frame = toFrameStart
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            fromView.frame = fromFrameEnd
+            toView.frame = frame
+        }, completion: { success in
+            completion(success)
+        })
+    }
+    
+    func getIndex(forViewController vc: UIViewController) -> Int? {
+        for (index, thisVC) in viewControllers.enumerated() {
+            if thisVC == vc { return index }
+        }
+        return nil
+    }
+}
+
+extension UIFont {
+    
+    func withTraits(traits: UIFontDescriptor.SymbolicTraits) -> UIFont {
+        let descriptor = fontDescriptor.withSymbolicTraits(traits)
+        return UIFont(descriptor: descriptor!, size: 0)
+    }
+    
+    func bold() -> UIFont {
+        return withTraits(traits: .traitBold)
+    }
+}
+
+extension UIViewController {
+    
+    func add(_ child: UIViewController) {
+        addChild(child)
+        view.addSubview(child.view)
+        child.didMove(toParent: self)
+    }
+    
+    func remove() {
+        guard parent != nil else { return }
+        willMove(toParent: nil)
+        view.removeFromSuperview()
+        removeFromParent()
+    }
+}
+
